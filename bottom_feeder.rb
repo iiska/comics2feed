d47cf23b19f56638594cfb42469573428ca3970a
Juhamatti Niemelä <iiska@iki.fi>
2010-03-30 13:20:45 +0300

    More functionality

--- bottom_feeder.rb
+++ bottom_feeder.rb
@@ -1,20 +1,53 @@
 #! /usr/bin/env ruby
 
 require 'yaml'
+require 'openuri'
+
+require 'rubygems'
+require 'nokogiri'
 
 module BottomFeeder
   class Strip
     # 1. use link format as a erb template binded to this strip
-    def initialize(img_url, link, cache_dir)
+    # config = {'img_url' => ?, 'link' => ?, 'link_format' => ?}
+    def initialize(config)
     end
 
-    def cache()
-    end
   end
 
   class Comic
-    def initialize(config)
+    def initialize(name, config)
+      @name = name
+      @config = config
       p config
+    end
+
+    def parse_strip()
+      doc = Nokogiri::HTML(open(@config.url))
+      sc = {}
+      sc['img_url'] = doc.css(@config['image_selector']).first.src
+      if (@config['link_selector'])
+        sc['link'] = doc.css(@config['link_selector']).first.href
+      else
+        sc['link_format'] = @config['link_format']
+      end
+      Strip.new(sc)
+    end
+
+    def load_cache()
+      cache = YAML.load_file("#{@config['cache_dir']}/#{@name}.yml")
+      @strips = cache.map{|c|Strip.new(c)}
+    end
+
+    def write_cache()
+      cache = @strips.map{|s|s.config}
+
+    end
+
+    # Check if comic is updated and generate feed if needed.
+    def update()
+      n = parse_strip
+      if not @strips.include?(n)
     end
   end
 
@@ -28,7 +61,8 @@ module BottomFeeder
     def initialize(config)
       @comics = []
       config['comics'].each do |k,v|
-        @comics << BottomFeeder::Comic.new(v)
+        v['cache_dir'] = config['cache_dir']
+        @comics << BottomFeeder::Comic.new(k,v)
       end
     end
   end
