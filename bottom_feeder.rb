6b4faa46175b7f6dfdc05d939f9de0cab781b3d7
Juhamatti Niemelä <iiska@iki.fi>
2010-03-30 10:27:47 +0300

    Simple config file parsing.

--- bottom_feeder.rb
+++ bottom_feeder.rb
@@ -2,31 +2,44 @@
 
 require 'yaml'
 
-class BottomFeeder::Strip
-  # 1. use link format as a erb template binded to this strip
-end
+module BottomFeeder
+  class Strip
+    # 1. use link format as a erb template binded to this strip
+    def initialize(img_url, link, cache_dir)
+    end
 
-class BottomFeeder::Comic
-  def initialize(config)
+    def cache()
+    end
+  end
+
+  class Comic
+    def initialize(config)
+      p config
+    end
+  end
+
+  class Base
+    # 1. Loop through configurated comics
+    # 2. Load comic url
+    #    - Parse site, find link and image
+    #    - If link not found use format string
+    # 3. Add to cache if not found
+    # 4. Update rss feed if needed
+    def initialize(config)
+      @comics = []
+      config['comics'].each do |k,v|
+        @comics << BottomFeeder::Comic.new(v)
+      end
+    end
   end
 end
 
-class BottomFeeder::Base
-  # 1. Loop through configurated comics
-  # 2. Load comic url
-  #    - Parse site, find link and image
-  #    - If link not found use format string
-  # 3. Add to cache if not found
-  # 4. Update rss feed if needed
-  def initialize(config)
-  end
-end
-
-def find_config_file(script_dir)
-  # 1. ~/.bottom_feeder.yml
-  # 2. config.yml
+def find_config_file()
+  ["~/.bottom_feeder.yml", "config.yml"].select{|f|
+    File.exist?(f)
+  }.first
 end
 
 if $0 == __FILE__
-
+  bf = BottomFeeder::Base.new(YAML.load_file(find_config_file))
 end
