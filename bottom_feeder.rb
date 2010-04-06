7ec795ea03b4bf441129089c33c05e8d0114d6e5
Juhamatti Niemelä <iiska@iki.fi>
2010-04-06 17:38:16 +0300

    RSS generation and strip caching.

--- bottom_feeder.rb
+++ bottom_feeder.rb
@@ -1,16 +1,32 @@
 #! /usr/bin/env ruby
 
 require 'yaml'
-require 'openuri'
+require 'erb'
+require 'uri'
+require 'open-uri'
 
 require 'rubygems'
 require 'nokogiri'
 
 module BottomFeeder
+
   class Strip
+    attr_reader :config
+    attr_reader :link
+    attr_reader :img_url
+    attr_reader :title
     # 1. use link format as a erb template binded to this strip
     # config = {'img_url' => ?, 'link' => ?, 'link_format' => ?}
     def initialize(config)
+      @title = config['title']
+      @img_url = config['img_url']
+      @img_basename = File.basename URI.parse(config['img_url']).path, ".*"
+      @link = config['link'] ||
+        ERB.new(config['link_format']).result(binding)
+    end
+
+    def to_yaml(e)
+      {'link' => @link, 'title' => @title, 'img_url' => @img_url}.to_yaml(e)
     end
 
   end
@@ -18,16 +34,18 @@ module BottomFeeder
   class Comic
     def initialize(name, config)
       @name = name
+      @url = config['url']
       @config = config
-      p config
+      load_cache
     end
 
     def parse_strip()
-      doc = Nokogiri::HTML(open(@config.url))
+      doc = Nokogiri::HTML(open(@url))
       sc = {}
-      sc['img_url'] = doc.css(@config['image_selector']).first.src
+      sc['title'] = doc.css("title").text
+      sc['img_url'] = doc.css(@config['image_selector']).attr('src').value
       if (@config['link_selector'])
-        sc['link'] = doc.css(@config['link_selector']).first.href
+        sc['link'] = doc.css(@config['link_selector']).attr('href').value
       else
         sc['link_format'] = @config['link_format']
       end
@@ -35,13 +53,19 @@ module BottomFeeder
     end
 
     def load_cache()
-      cache = YAML.load_file("#{@config['cache_dir']}/#{@name}.yml")
-      @strips = cache.map{|c|Strip.new(c)}
+      f = "#{@config['cache_dir']}/#{@name}.yml"
+      if File.exist?(f)
+        cache = YAML.load_file(f)
+        @strips = cache.map{|c|Strip.new(c)} || []
+      else
+        @strips = []
+      end
     end
 
     def write_cache()
-      cache = @strips.map{|s|s.config}
-      # Write to file
+      File.open("#{@config['cache_dir']}/#{@name}.yml", "w") do |f|
+        f.puts @strips.to_yaml
+      end
     end
 
     # Check if comic is updated and generate feed if needed.
@@ -55,6 +79,10 @@ module BottomFeeder
     end
 
     def generate_rss
+      template = ERB.new(File.open("rss.xml.erb","r").read)
+      File.open("#{@name}.xml", "w") do |f|
+        f.puts(template.result(binding))
+      end
     end
   end
 
@@ -66,6 +94,7 @@ module BottomFeeder
     # 3. Add to cache if not found
     # 4. Update rss feed if needed
     def initialize(config)
+      puts config.to_yaml
       @comics = []
       config['comics'].each do |k,v|
         v['cache_dir'] = config['cache_dir']
