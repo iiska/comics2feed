e126961016afdcc3be97ed775cc72c28155feaa3
Juhamatti Niemelä <iiska@iki.fi>
2010-09-12 13:36:41 +0300

    Include date to the feeds.

--- bottom_feeder.rb
+++ bottom_feeder.rb
@@ -16,12 +16,14 @@ module BottomFeeder
     attr_reader :link
     attr_reader :img_url
     attr_reader :title
+    attr_reader :date
     # 1. use link format as a erb template binded to this strip
     # config = {'img_url' => ?, 'link' => ?, 'link_format' => ?}
     def initialize(config)
       @title = config['title']
       @img_url = config['img_url']
       @img_basename = File.basename URI.parse(config['img_url']).path, ".*"
+      @date = config['date']
       @link = config['link'] ||
         ERB.new(config['link_format']).result(binding)
     end
@@ -43,7 +45,8 @@ module BottomFeeder
     end
 
     def to_yaml(e)
-      {'link' => @link, 'title' => @title, 'img_url' => @img_url}.to_yaml(e)
+      {'link' => @link, 'title' => @title, 'img_url' => @img_url,
+        'date' => @date}.to_yaml(e)
     end
 
   end
@@ -66,6 +69,7 @@ module BottomFeeder
       else
         sc['link_format'] = @config['link_format']
       end
+      sc['date'] = Time.now
       Strip.new(sc)
     end
 
